5bc4cc8243ecf539f17f4a9e78d97c352ef95191
Carlos Ribeiro <carlosribeiroeduardo12@gmail.com>
2026-09-20 17:57:51 -0300

    Estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+# Projeto Elo Comum
+
+💡 Proposta do Projeto
+
+O Elo Comum é uma plataforma web desenvolvida para incentivar o consumo colaborativo, permitindo que pessoas compartilhem, doem, emprestem ou troquem itens dentro de uma comunidade.
+
+A plataforma busca reduzir desperdícios, estimular a reutilização de recursos e fortalecer a colaboração entre pessoas, alinhando-se aos Objetivos de Desenvolvimento Sustentável (ODS) da ONU, especialmente:
+
+ODS 11 – Cidades e Comunidades Sustentáveis
+ODS 12 – Consumo e Produção Responsáveis
+ODS 13 – Ação Contra a Mudança do Clima
+
+O sistema permitirá que usuários publiquem itens disponíveis, encontrem produtos próximos de sua necessidade e realizem solicitações de interesse de forma simples e organizada.
---
+++ database/migrations/2026_05_23_212818_create_item_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained('items')->cascadeOnDelete();
+            $table->foreignId('requester_id')->constrained('users')->cascadeOnDelete();
+
+            $table->enum('status', [
+                'pending',
+                'approved',
+                'rejected'
+            ])->default('pending');
+
+            $table->text('message')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_requests');
+    }
+};
